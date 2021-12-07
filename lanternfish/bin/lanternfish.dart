24cd8e77f650a1184fe3ae046e16d75bbc7d7c74
Patrick Martin <pux1987@gmail.com>
2021-12-06 22:03:32 -0700

    Finished part 1

--- lanternfish/bin/lanternfish.dart
+++ lanternfish/bin/lanternfish.dart
@@ -1,3 +1,28 @@
-import 'package:lanternfish/lanternfish.dart' as lanternfish;
+import 'dart:io';
 
-void main(List<String> arguments) {}
+import 'package:args/args.dart';
+import 'package:lanternfish/lanternfish.dart';
+
+Future<void> main(List<String> arguments) async {
+  var parser = ArgParser()
+    ..addOption("input",
+        abbr: "i", mandatory: true, help: "The input file to read")
+    ..addOption("days",
+        abbr: "d", help: "The number of days to simulate. Exclude for 0");
+
+  String inputFile;
+  int days;
+  try {
+    var result = parser.parse(arguments);
+    inputFile = result['input'];
+    days = int.tryParse(result['days']) ?? 0;
+  } catch (e) {
+    print(parser.usage);
+    return;
+  }
+
+  var input = await File(inputFile).readAsLines();
+  var lanternfish = Lanternfish.parse(input[0]);
+  lanternfish.advance(days);
+  print("Count: ${lanternfish.state.length}");
+}
