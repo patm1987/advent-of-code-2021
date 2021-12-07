8d3cd99f0d66b65ccc0668d7e204f7bbd7d3a5fb
Patrick Martin <pux1987@gmail.com>
2021-12-06 21:32:53 -0700

    Created an empty project for the lanternfish

---
+++ lanternfish/bin/lanternfish.dart
@@ -0,0 +1,5 @@
+import 'package:lanternfish/lanternfish.dart' as lanternfish;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lanternfish.calculate()}!');
+}
---
+++ lanternfish/test/lanternfish_test.dart
@@ -0,0 +1,8 @@
+import 'package:lanternfish/lanternfish.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
