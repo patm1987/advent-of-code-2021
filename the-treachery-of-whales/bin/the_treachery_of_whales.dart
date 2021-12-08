5a62224f3bbd4088ec6446eb6c249a992a223d4d
Patrick Martin <pux1987@gmail.com>
2021-12-07 22:21:57 -0700

    Created the command line version

--- the-treachery-of-whales/bin/the_treachery_of_whales.dart
+++ the-treachery-of-whales/bin/the_treachery_of_whales.dart
@@ -1,5 +1,15 @@
-import 'package:the_treachery_of_whales/the_treachery_of_whales.dart' as the_treachery_of_whales;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${the_treachery_of_whales.calculate()}!');
+import 'package:the_treachery_of_whales/crab_swarm.dart';
+
+Future<void> main(List<String> arguments) async {
+  if (arguments.isEmpty) {
+    print("Please specify an input file");
+    return;
+  }
+
+  var lines = await File(arguments[0]).readAsLines();
+  var crabSwarm = CrabSwarm.parse(lines[0]);
+  print("Optimal Position: ${crabSwarm.optimalPosition}");
+  print("Optimal Cost: ${crabSwarm.optimalCost}");
 }
